eb34fd733146b6515b63e0dc2235631413b9648d
JehanKandy <jehankandy@gmail.com>
2023-01-03 08:57:47 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -4,11 +4,19 @@
 <div class="container">
     <div class="card login-card">
     <div class="card-header">
-        Login
+        Sign up
     </div>
         <div class="card-body">
+            <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
+                <p class="card-text">Username : </p>
+                <input type="text" name="username" id="" class="form-control form-input">
 
-            
+                <p class="card-text">Email : </p>
+                <input type="email" name="email" id="" class="form-control form-text">
+
+                
+            </form>
+
         </div>
     </div>
 </div>
