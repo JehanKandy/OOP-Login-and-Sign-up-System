27c192495869ae6cad2a2dcb7405c715da0eccf0
JehanKandy <jehankandy@gmail.com>
2023-01-03 08:55:07 +0530

    Add files via upload

---
+++ lib/views/reg.php
@@ -0,0 +1,16 @@
+<?php include("../layouts/header.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
+
+<div class="container">
+    <div class="card login-card">
+    <div class="card-header">
+        Login
+    </div>
+        <div class="card-body">
+
+            
+        </div>
+    </div>
+</div>
+
+<?php include("../layouts/footer.php"); ?>
