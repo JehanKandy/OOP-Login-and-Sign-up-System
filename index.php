217b7cbf5a453a184e89e2795f2c88ccdab60d96
JehanKandy <jehankandy@gmail.com>
2023-01-03 08:35:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include("lib/layouts/header.php"); ?>
+<link rel="stylesheet" href="css/style.css">
+
+<div class="home-content">
+    <div class="container">
+        OOP Login From
+        <br>
+        <a href="lib/views/login.php"><button class="btn btn-primary">Login Here</button></a>
+    </div>
+</div>
+
+
+
+<?php include("lib/layouts/footer.php"); ?>
---
+++ lib/layouts/footer.php
@@ -0,0 +1,8 @@
+<?php include("header.php"); ?>
+<link rel="stylesheet" href="../css/style.css">
+
+<div class="footer">
+    <div class="container">
+        <div class="content"><a href="https://github.com/JehanKandy" target="blank" style="text-decoration: none;"> Jehankandy</a> - OOP Login</div>
+    </div>
+</div>
