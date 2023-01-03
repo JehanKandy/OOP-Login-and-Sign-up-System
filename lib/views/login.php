b91577de252cf5c76b9462b7287cc1b239201011
JehanKandy <jehankandy@gmail.com>
2023-01-03 08:48:48 +0530

    Update login.php

--- lib/views/login.php
+++ lib/views/login.php
@@ -1,4 +1,17 @@
 <?php include("../layouts/header.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
 
+<div class="container">
+    <div class="card login-card">
+    <div class="card-header">
+        Login
+    </div>
+    <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    </div>
+</div>
 
 <?php include("../layouts/footer.php"); ?>
