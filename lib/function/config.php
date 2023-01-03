be086ffcf3f15355353eca8941c0844fe3da01f5
JehanKandy <jehankandy@gmail.com>
2023-01-03 21:15:26 +0530

    Update config.php

--- lib/function/config.php
+++ lib/function/config.php
@@ -4,5 +4,11 @@
     define('PASS','');
     define('DB_NAME','oop_login');
 
+    try{
+        $conn = new PDO("mysql:host=".HOST.";dbname=".DB_NAME, USER, PASS);
+    }
 
+    catch (PDOException $e){
+        exit("ERROR: ". $e->getMessage());
+    }
 ?>
