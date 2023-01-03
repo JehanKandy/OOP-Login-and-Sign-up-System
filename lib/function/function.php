79c2ce8bef67a0e5bc4fe578d53927129898e451
JehanKandy <jehankandy@gmail.com>
2023-01-03 21:29:11 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -1,26 +1,4 @@
 <?php 
-    $server = "localhost";
-    $user = "root";
-    $pass = "";
-    $db_name = "oop_login";
-    
-    try {
-        $con = new PDO("mysql:host=$server;dbname=$db_name", $user, $pass);
-        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $con->beginTransaction();
-
-        function reg_user($usernmae,$email,$pass,$cpass){
-            echo "hi";
-
-        }
-
-    }
-    catch(PDOException $e){
-        $con->rollBack();
-        echo "ERROR: " . $e->getMessage();
-    }
-
-    $con=null;
+    require_once("config.php");
 
 ?>
